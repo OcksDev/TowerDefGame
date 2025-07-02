7a456a630a7e784137121bf1e30137b181c19f98
Ocks <milomgenova@gmail.com>
2025-07-02 01:53:23 -0400

    some base game data stuff

---
+++ Assets/OcksTools/General/GameHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public static GameHandler Instance;
+    public Map Map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void SetMap()
+    {
+
+    }
+
+    public void SpawnEnemyWave()
+    {
+
+    }
+
+}
